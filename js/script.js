fed50d6aaf60fafef2abfa4cf4f2c4ddb5eee42a
Kasidit <kasiditza2546@gmail.com>
2025-04-10 20:26:15 +0700

    Add initial HTML structure, CSS and JavaScript functionality

---
+++ js/script.js
@@ -0,0 +1,6 @@
+function printMessage(){
+    document.getElementById("demo").innerHTML = "Hi JavaScript2025 To JavaScript2026";
+    document.getElementById("demo").style.color = "red";
+    document.getElementById("demo").style.fontSize = "15px";
+    document.getElementById("demo").style.fontFamily = "Arial";
+}
