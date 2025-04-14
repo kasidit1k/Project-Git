358efb47ff509563f9f8e917a1421a1e51c91352
Kasidit <kasiditza2546@gmail.com>
2025-04-15 00:15:29 +0700

    Update index.html and script.js mofify demo message and button functionality

--- js/script.js
+++ js/script.js
@@ -1,6 +1,4 @@
-function printMessage(){
-    document.getElementById("demo").innerHTML = "Hi JavaScript2025 To JavaScript2026";
-    document.getElementById("demo").style.color = "red";
-    document.getElementById("demo").style.fontSize = "15px";
-    document.getElementById("demo").style.fontFamily = "Arial";
+function displayDemo() {
+    document.getElementById("demo").innerHTML = "Hello World";
+    document.getElementById("demo").style.color = "blue";
 }
